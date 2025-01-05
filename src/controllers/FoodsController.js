df39dbca6816c23a497c40c793683e38363e74e5
luigi <luigigames2903@gmail.com>
2025-01-04 22:14:46 -0300

    rotas separando o show de cada tipo de prato

--- src/controllers/FoodsController.js
+++ src/controllers/FoodsController.js
@@ -1,3 +1,4 @@
+const { query } = require("express")
 const knex = require("../database/knex")
 const appError = require("../utils/AppError")
 
@@ -24,19 +25,20 @@ class FoodsController {
     return response.status(201).json()
   }
   async index(request, response) {
-    const {name} = request.body;
-
-    let foods;
-    let drinks;
-    let desserts;
+    const { name } = request.query
+    let foods
+    let drinks
+    let desserts
 
     foods = await knex("foods").whereLike("name", `%${name}%`).orderBy("name")
     drinks = await knex("drinks").whereLike("name", `%${name}%`).orderBy("name")
-    desserts = await knex("desserts").whereLike("name", `%${name}%`).orderBy("name")
+    desserts = await knex("desserts")
+      .whereLike("name", `%${name}%`)
+      .orderBy("name")
 
     const resultFoods = foods.map((food) => {
       return {
-        ...food
+        ...food,
       }
     })
     const resultDrinks = drinks.map((drink) => {
@@ -50,17 +52,54 @@ class FoodsController {
       }
     })
 
-    return response.json([resultFoods, resultDrinks, resultDesserts] )
+    return response.json([resultFoods, resultDrinks, resultDesserts])
+  }
+  async show(request, response) {
+    const { name } = request.query
+    console.log(name)
+    let foods
+
+    foods = await knex("foods").where({ name })
+    const resultFoods = foods.map((food) => {
+      return {
+        ...food,
+      }
+    })
+    console.log(resultFoods)
+    return response.json(resultFoods)
+  }
+  async showDrink(request, response) {
+    const { name } = request.query
+    let drinks
+
+    drinks = await knex("drinks").where({ name })
+    const resultDrinks = drinks.map((drink) => {
+      return {
+        ...drink,
+      }
+    })
+    return response.json(resultDrinks)
+  }
+  async showDessert(request, response) {
+    const { name } = request.query
+    let desserts
+
+    desserts = await knex("desserts").where({ name })
+    const resultDesserts = desserts.map((dessert) => {
+      return {
+        ...dessert,
+      }
+    })
+    return response.json(resultDesserts)
   }
   async delete(request, response) {
-    const {name} = request.body;
-    
-    await knex("foods").where({name}).delete();
-    await knex("drinks").where({ name }).delete();
+    const { name } = request.body
+
+    await knex("foods").where({ name }).delete()
+    await knex("drinks").where({ name }).delete()
     await knex("desserts").where({ name }).delete()
 
     return response.json(`${name} excluido`)
-
   }
 }
 
